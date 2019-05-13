c4987b6c3b85c63e8c4ec61157a90e8437b34d69
Artem Borisov <dedsa2002@gmail.com>
2019-05-14 00:38:49 +0530

    tissot: Fix USB config extra property
    * It should be persist.vendor.usb.config.extra, not persist.vendor.sys.usb.config.extra
    
    Change-Id: Icc5531c5563f1975314be38f5f7567e1c0f5eb8d
    Signed-off-by: Albert I <kras@raphielgang.org>

--- prop.mk
+++ prop.mk
@@ -7,10 +7,17 @@ ro.vendor.audio.sdk.fluencetype=fluence
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.camera.aux.packageblacklist=com.discord \
-persist.camera.HAL3.enabled=1 \
-vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera2,com.google.android.GoogleCamera \
-vendor.camera.aux.packagelist2=com.google.android.GoogleCameraWide,com.dual.GCam,com.Wide.GCam,com.Tele.GCam \
-vendor.camera.hal1.packagelist=com.whatsapp \
-persist.camera.dual.camera=0 \
-persist.camera.eis.enable=1
+camera.display.lmax=1280x720 \
+camera.display.umax=1920x1080 \
+camera.lowpower.record.enable=1 \
+media.camera.ts.monotonic=1 \
+persist.camera.gyro.disable=0 \
+persist.camera.isp.clock.optmz=0 \
+persist.camera.stats.test=5 \
+persist.vendor.qti.telephony.vt_cam_interface=1 \
+vidc.enc.dcvs.extra-buff-count=2
+
+# Usb
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.usb.config.extra=none
+
