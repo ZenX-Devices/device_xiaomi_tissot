237b57d62f678822d8598dd8ce89b5d70048adfc
CatzBurry <myanonymoussad@gmail.com>
2020-09-04 18:29:23 +0700

    tissot: Fix Fps path
    * Do not include GApps
    
    Signed-off-by: CatzBurry <myanonymoussad@gmail.com>

--- zenx_tissot.mk
+++ zenx_tissot.mk
@@ -47,6 +47,5 @@ BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:u
 #Bleed Zenx
 ZENX_BUILD_TYPE := Official
 TARGET_GAPPS_ARCH := arm64
-WITH_GAPPS := true
 IS_PHONE := true
 TARGET_BOOT_ANIMATION_RES := 1080
